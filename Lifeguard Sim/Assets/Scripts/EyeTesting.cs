b0b8df035dbe48f1af015d9963341981c3429d0d
Quinn Daggett <quinn.daggett@uoit.net>
2019-09-29 19:38:37 -0400

    Eye tracking and UI point
    + Added script for tracking player's gaze with tobii eye tracker
    + Added canvas and UI dot

---
+++ Lifeguard Sim/Assets/Scripts/EyeTesting.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Used for sprite tracking player's gaze position
+using Tobii.Gaming; // This is necessary for using the Tobii SDK functions
+using TMPro; // Used for TextMeshPro
+
+public class EyeTesting : MonoBehaviour
+{
+    [SerializeField]
+    private Camera viewCamera; // Player's viewport camera
+    private bool trackerConnected; // For checking if eye tracking is connected
+    private Vector2 smoothPoint; // Smoothing player's gaze position
+    [SerializeField]
+    private Image gazePosImage; // Image showing where player is looking
+
+    [SerializeField]
+    private TextMeshProUGUI trackerText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TobiiAPI.SetCurrentUserViewPointCamera(viewCamera); // Setting view camera for Tobii eye tracking
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trackerConnected = TobiiAPI.IsConnected; // Status of tobii eye tracker
+
+        if(trackerConnected) // Tracker is connected and functioning properly
+        {
+            EyeTracking(); // Update eye tracking every frame
+            trackerText.gameObject.SetActive(false);
+        }
+
+        else // Something is wrong with tobii eye tracker
+        { 
+            Time.timeScale = 0f; // Pause game
+            trackerText.gameObject.SetActive(true); // Inform user that eye tracking is not functioning
+        }
+
+    }
+
+    private void EyeTracking() // Function for updating eye tracking for player
+    {
+        Vector2 gazePoint = TobiiAPI.GetGazePoint().Screen; // Get where player is looking in viewport coordinates
+
+        smoothPoint = Vector2.Lerp(smoothPoint, gazePoint, 0.5f); // Smoothing player's gaze point
+
+        gazePosImage.transform.position = new Vector3(smoothPoint.x, smoothPoint.y, 0f);
+    }
+}
