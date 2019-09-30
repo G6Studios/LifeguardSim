207a3df2e97015fb1f6efeba64b2b84543723d9a
Quinn Daggett <quinn.daggett@uoit.net>
2019-09-30 18:41:45 -0400

    Eye tracking warning
    + Added toggle boolean to eye tracker connectivity warning

--- Lifeguard Sim/Assets/Scripts/EyeTesting.cs
+++ Lifeguard Sim/Assets/Scripts/EyeTesting.cs
@@ -8,6 +8,9 @@ using TMPro; // Used for TextMeshPro
 public class EyeTesting : MonoBehaviour
 {
     [SerializeField]
+    private bool toggleTrackerWarning;
+
+    [SerializeField]
     private Camera viewCamera; // Player's viewport camera
     private bool trackerConnected; // For checking if eye tracking is connected
     private Vector2 smoothPoint; // Smoothing player's gaze position
@@ -15,7 +18,7 @@ public class EyeTesting : MonoBehaviour
     private Image gazePosImage; // Image showing where player is looking
 
     [SerializeField]
-    private TextMeshProUGUI trackerText;
+    private GameObject trackerWarning; // Canvas element that informs players when eye tracking is not working
     
     // Start is called before the first frame update
     void Start()
@@ -27,17 +30,23 @@ public class EyeTesting : MonoBehaviour
     void Update()
     {
         trackerConnected = TobiiAPI.IsConnected; // Status of tobii eye tracker
-
+        
         if(trackerConnected) // Tracker is connected and functioning properly
         {
             EyeTracking(); // Update eye tracking every frame
-            trackerText.gameObject.SetActive(false);
+            trackerWarning.SetActive(false);
+        }
+
+        else if(toggleTrackerWarning == false)
+        {
+            EyeTracking(); // Update eye tracking every frame
+            trackerWarning.SetActive(false);
         }
 
         else // Something is wrong with tobii eye tracker
         { 
             Time.timeScale = 0f; // Pause game
-            trackerText.gameObject.SetActive(true); // Inform user that eye tracking is not functioning
+            trackerWarning.SetActive(true); // Inform user that eye tracking is not functioning
         }
 
     }
